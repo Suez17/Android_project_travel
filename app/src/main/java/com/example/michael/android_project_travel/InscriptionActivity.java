4a10e29df97706d2cee507edd92d37a22fd72256
KevinAgesilas <k.agesilas@laposte.net>
2017-05-21 23:18:12 +0200

    Ajout des fenêtres suivantes : --> Connexion --> Inscription --> Création groupe

---
+++ app/src/main/java/com/example/michael/android_project_travel/InscriptionActivity.java
@@ -0,0 +1,13 @@
+package com.example.michael.android_project_travel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription_activity);
+    }
+}
--- app/src/main/java/gestion/contact/TestContact.java
+++
@@ -1,8 +0,0 @@
-package gestion.contact;
-
-/**
- * Created by Isidore on 18/05/2017.
- */
-
-public class TestContact {
-}
